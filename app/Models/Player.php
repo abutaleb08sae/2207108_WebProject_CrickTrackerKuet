3c1229e0744657e9dc7d7e7bdd2123cceca76e26
abutaleb08sae <sardar2207108@stud.kuet.ac.bd>
2026-07-10 10:37:20 +0600

    Add database table team and player

--- app/Models/Player.php
+++ app/Models/Player.php
@@ -3,8 +3,17 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Player extends Model
 {
-    //
+    protected $fillable = [
+        'team_id', 'name', 'role', 'student_id', 
+        'matches_played', 'total_runs', 'total_wickets'
+    ];
+
+    public function team(): BelongsTo
+    {
+        return $this->belongsTo(Team::class);
+    }
 }
--- app/Models/Team.php
+++ app/Models/Team.php
@@ -3,8 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Team extends Model
 {
-    //
+    protected $fillable = ['name', 'logo', 'club_history'];
+
+    public function players(): HasMany
+    {
+        return $this->hasMany(Player::class);
+    }
 }
